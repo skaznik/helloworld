16a4d3b550584672ecb7e3f248923a331c83067f
skaznik <57837823+skaznik@users.noreply.github.com>
2019-11-16 16:32:51 +0100

    Update HelloWorld.java

--- src/HelloWorld.java
+++ src/HelloWorld.java
@@ -1,5 +1,8 @@
 public class HelloWorld {
     public static void main(String[] args) {
+        
         System.out.println("Witaj świecie");
+        
+        System.out.println("Witaj świecie po raz drugi");
     }
 }
